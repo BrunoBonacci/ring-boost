54c9716493993ffad79fe0b13f5094c9f27bba31
Bruno Bonacci <bruno.bonacci@gmail.com>
2018-04-15 16:45:43 +0100

    added default configuration

--- examples/fib/src/fib/handler.clj
+++ examples/fib/src/fib/handler.clj
@@ -2,9 +2,8 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.middleware.json :refer [wrap-json-response]]
-            [com.brunobonacci.ring-boost :refer [ring-boost]]
-            [com.brunobonacci.ring-boost.core :as bc]))
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [com.brunobonacci.ring-boost :refer [ring-boost]]))
 
 
 ;; Purposely not the most efficient implementation
@@ -28,33 +27,32 @@
 
 
 
+(defroutes app-routes
+  (GET "/fib/:pos" [pos]
+       (let [position (parse-num pos)]
+         (if (and position (>= position 0))
+           {:status 200 :body {:status "OK" :pos position :fib (fib position)}}
+           {:status 400 :body {:status "ERROR"
+                               :message "Invalid position number. Please provide a positive integer."}})))
+
+  (route/not-found "Not Found"))
+
+
+
 (def boost-config
-  {:enabled true
-   :storage {:sophia.path "/tmp/ring-boost-cache" :dbs ["cache" "stats"]}
-   :profiles
+  {:profiles
    [{:profile :fib-numbers
      :match [:and
              [:uri :starts-with? "/fib/"]
              [:request-method = :get]]
      ;; cache for 30 sec
-     :cache-for 30}]
-   :processor-seq bc/default-processor-seq})
-
-
-
-(defroutes app-routes
-  (GET "/fib/:pos" [pos]
-       (let [position (parse-num pos)]
-         (if (and position (>= position 0))
-           {:status 200 :body {:status "OK" :pos pos :fib (fib position)}}
-           {:status 400 :body {:status "ERROR"
-                               :message "Invalid position number. Please provide a positive integer."}})))
-  (route/not-found "Not Found"))
+     :cache-for 30}]})
 
 
 
 (def app
   (-> app-routes
       (wrap-json-response)
+      (wrap-json-body {:keywords? true})
       ;; add ring-boost
       (ring-boost boost-config)))
