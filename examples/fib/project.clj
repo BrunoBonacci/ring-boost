29bbcf64e11980053808c4bad13ee23cb8749d05
altimario <altimario@gmail.com>
2018-04-18 16:39:08 +0100

    just a test

--- examples/fib/project.clj
+++ examples/fib/project.clj
@@ -6,7 +6,7 @@
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
                  [ring/ring-json "0.4.0"]
-                 [com.brunobonacci/ring-boost "0.1.0-SNAPSHOT"]
+                 [com.brunobonacci/ring-boost "0.1.0"]
                  [org.slf4j/slf4j-log4j12 "1.7.25"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler fib.handler/app}
