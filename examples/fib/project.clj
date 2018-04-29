49fb93ead02bf47d3d66e144c6df9ac7a30b688e
Bruno Bonacci <bruno.bonacci@gmail.com>
2018-04-29 15:27:29 +0100

    release: v0.1.1 - fix concurrent stats update

--- examples/fib/project.clj
+++ examples/fib/project.clj
@@ -1,4 +1,4 @@
-(defproject fib "0.1.0"
+(defproject fib "0.1.1"
   :description "Sample project for ring-boost"
   :url "https://github.com/BrunoBonacci/ring-boost"
   :min-lein-version "2.0.0"
@@ -6,7 +6,7 @@
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
                  [ring/ring-json "0.4.0"]
-                 [com.brunobonacci/ring-boost "0.1.0"]
+                 [com.brunobonacci/ring-boost "0.1.1"]
                  [org.slf4j/slf4j-log4j12 "1.7.25"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler fib.handler/app}
