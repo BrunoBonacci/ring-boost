e442fb5da2d43841543da74247edb62bc0152687
Bruno Bonacci <bruno.bonacci@gmail.com>
2018-05-24 22:57:44 +0100

    updated clj-sophia version

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
   :scm {:name "git" :url "https://github.com/BrunoBonacci/ring-boost.git"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.brunobonacci/clj-sophia "0.4.3"]
+                 [com.brunobonacci/clj-sophia "0.4.4"]
                  [com.brunobonacci/where "0.5.0"]
                  [pandect "0.6.1"]]
 
